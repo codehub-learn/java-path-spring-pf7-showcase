f6d446390ec1ad89bd7c16a15a5ed52fbb830d94
Christos Peristeris <chrisperis@gmail.com>
2021-12-13 23:01:59 +0200

    Add Spring profile examle, containing profile property file, and a @Profile annotated bean

--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/bootstrap/SampleRunner.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/bootstrap/SampleRunner.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @RequiredArgsConstructor
+@Profile("dev")
 public class SampleRunner implements CommandLineRunner {
 
 	private final Logger logger = LoggerFactory.getLogger(SampleRunner.class);
@@ -32,18 +34,21 @@ public class SampleRunner implements CommandLineRunner {
 	@Value("${default.example:default}")
 	private String defaultValueExample;
 
+	@Value("${profile.property.example}")
+	private String profilePropertyExample;
+
 	@Override
 	public void run(final String... args) throws Exception {
 		logger.info("Application named '{}' is being started at port {} " +
 							"has a custom property having the value of '{}' " +
-							"as well as a property getting a default value '{}' ", applicationName, serverPort,
-					customProperty, defaultValueExample);
+							"as well as a property getting a default value '{}' " +
+							"and a profile based property with value '{}' ",
+					applicationName, serverPort, customProperty, defaultValueExample, profilePropertyExample);
 
 
 		// Retrieves information from YAML file
 		logger.info("Using yaml, current threadPool value is {}.", mappedConfigurationProperties.getThreadPool());
 		logger.info("Using yaml, current email value is {}.", mappedConfigurationProperties.getEmail());
 		logger.info("Using yaml, current server values are {}.", mappedConfigurationProperties.getServers());
-
 	}
 }
