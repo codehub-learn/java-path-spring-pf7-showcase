c2701bc9dc03e08d2915923301f059a3b54adb79
Christos Peristeris <chrisperis@gmail.com>
2021-12-13 22:25:58 +0200

    Add sample controller for demonstrating application property usage

---
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/controller/SampleController.java
@@ -0,0 +1,17 @@
+package gr.codelearn.spring.showcase.core.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello from our sample Spring Boot with name: " + applicationName;
+	}
+}
--- java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/domain/Bike.java
+++ java-path-spring-core/src/main/java/gr/codelearn/spring/showcase/core/domain/Bike.java
@@ -2,8 +2,6 @@ package gr.codelearn.spring.showcase.core.domain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
