f3d46bc7e1fc2c7abdf6bbb3cc6d99aa1a969cb3
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2021-12-22 14:54:00 +0200

    Added our intention to not modify the id attribute once initialized to a value.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/domain/BaseModel.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/domain/BaseModel.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,5 +21,6 @@ public class BaseModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idGenerator")
+	@Column(updatable = false)
 	private Long id;
 }
